b8c3819f5035dbb6a56dfc82e97caa136eb05ca2
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-10 17:05:00 +0500

    done

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,25 @@
-from user.models import User
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from .forms import UserAdminCreateForm
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'full_name')
+class UserAdmin(BaseUserAdmin):
+    add_form = UserAdminCreateForm
+    model = User
+
+    list_display = ("id", "username", "role", "is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Info", {"fields": ("full_name", "phone_number", "role")}),
+        ("Permissions", {"fields": ("is_staff", "is_superuser", "is_active")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "password"),
+        }),
+    )
