2458c193906a75a1d9a5d3ffbbe34761ed7665a4
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-10 16:17:59 +0500

    done

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from user.views.user_views import UserViewSet
 from rest_framework.routers import DefaultRouter
-from user.views.auth_views import SignInAPIView, RefreshTokenAPIView, MeAPIView
+from user.views.auth_views import SignInAPIView, RefreshTokenAPIView, MeAPIView, LogOutAPIView
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
@@ -9,6 +9,7 @@ router.register('users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', SignInAPIView.as_view(), name='login'),
+    path('logout/', LogOutAPIView.as_view(), name='logout'),
     path('auth/refresh/', RefreshTokenAPIView.as_view(), name='token_refresh'),
     path('me/', MeAPIView.as_view(), name='me'),
 ]
