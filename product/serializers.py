8fc3d2837d3b93dd4ec382953c2e6fe47e5b81bb
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-12 13:14:42 +0500

    done

--- product/serializers.py
+++ product/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 from product.models import Product
-from utils.base.serializers_base import BaseReadSerializer
-
-
-class ManagerProductSerializer(BaseReadSerializer):
-    class Meta(BaseReadSerializer.Meta):
-        model = Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        exclude = ["arrival_price"]
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+
+        request = self.context.get("request")
+
+        if not request or request.user.role != request.user.UserRoles.MANAGER:
+            data.pop("arrival_price", None)
+
+        return data
--- product/views.py
+++ product/views.py
@@ -1,24 +1,17 @@
 from drf_spectacular.utils import extend_schema
 from product.models import Product
 from rest_framework import filters
-from product.serializers import ProductSerializer, ManagerProductSerializer
+from product.serializers import ProductSerializer
 from utils.base.views_base import BaseUserViewSet, User
 from django_filters.rest_framework import DjangoFilterBackend
 
 
 @extend_schema(tags=["Product"])
 class ProductViewSet(BaseUserViewSet):
-    ordering = ['-id']
+    queryset = Product.objects.select_related("category")
+    serializer_class = ProductSerializer
 
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['category', 'quality']
-    search_fields = ['name']
-
-    def get_queryset(self):
-        return Product.objects.select_related('category')
-
-    def get_serializer_class(self):
-        user = self.request.user
-        if user.role == User.UserRoles.MANAGER:
-            return ManagerProductSerializer
-        return ProductSerializer
+    filterset_fields = ["category", "quality"]
+    search_fields = ["name"]
+    ordering = ["-id"]
