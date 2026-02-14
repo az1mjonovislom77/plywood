e51e272d4961663c8567980cc1e73a43c40adb8d
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-14 20:40:36 +0500

    done

--- order/admin.py
+++ order/admin.py
@@ -1,4 +1,4 @@
-from order.models import Basket, Cutting
+from order.models import Basket, Cutting, Thickness, Banding, Order
 from django.contrib import admin
 
 
@@ -10,5 +10,20 @@ class CuttingAdmin(admin.ModelAdmin):
     list_display = ("id", "count")
 
 
+class ThicknessAdmin(admin.ModelAdmin):
+    list_display = ("id", "size", "price")
+
+
+class BandingAdmin(admin.ModelAdmin):
+    list_display = ("id", "width", "height")
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "payment_method")
+
+
+admin.site.register(Banding, BandingAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Thickness, ThicknessAdmin)
 admin.site.register(Basket, BasketAdmin)
 admin.site.register(Cutting, CuttingAdmin)
