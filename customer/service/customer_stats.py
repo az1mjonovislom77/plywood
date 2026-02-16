35d60254af7f14c5eff8e640f51b2f9f6eee0506
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-16 15:38:27 +0500

    done

--- customer/service/customer_stats.py
+++ customer/service/customer_stats.py
@@ -1,7 +1,7 @@
-from django.db.models import Count, Q
+from decimal import Decimal
+from django.db.models import Count, Q, Sum, Value, DecimalField
 from django.db.models.functions import Coalesce
 from customer.models import Customer
-from django.db.models import Sum, Value
 
 
 class CustomerStatsService:
@@ -11,7 +11,7 @@ class CustomerStatsService:
         stats = Customer.objects.aggregate(
             total_customers=Count("id"),
             debtor_customers=Count("id", filter=Q(debt__gt=0)),
-            total_debt=Coalesce(Sum("debt"), Value(0))
+            total_debt=Coalesce(Sum("debt"), Value(Decimal("0.00")), output_field=DecimalField())
         )
 
         return stats
