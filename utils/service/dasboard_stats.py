e0d04cc1ad24aa890e5c9b4a065ca64605dffb75
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-18 15:10:55 +0500

    done

--- utils/service/dasboard_stats.py
+++ utils/service/dasboard_stats.py
@@ -15,12 +15,16 @@ class DashboardStatsService:
 
         stats = Order.objects.aggregate(
             today_income=Coalesce(Sum("total_price", filter=Q(created_at__date=today)), Decimal("0.00")),
-            total_income=Coalesce(Sum("total_price"), Decimal("0.00"))
+            total_income=Coalesce(Sum("total_price"), Decimal("0.00")),
+            total_sales=Coalesce(Count("id"), 0),
+            total_discount=Coalesce(Sum("discount"), Decimal("0.00")),
         )
         total_products = Product.objects.count()
 
         return {
             "today_income": stats["today_income"],
             "total_income": stats["total_income"],
+            "total_sales": stats["total_sales"],
+            "total_discount": stats["total_discount"],
             "total_products": total_products
         }
