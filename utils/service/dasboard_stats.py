1ba443874f7b5be9e3b8b621f80b6861c3a27875
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-19 17:31:52 +0500

    done

--- utils/service/dasboard_stats.py
+++ utils/service/dasboard_stats.py
@@ -3,7 +3,7 @@ from django.db.models.functions import Coalesce
 from django.utils import timezone
 from decimal import Decimal
 
-from order.models import Order
+from order.models import Order, OrderItem
 from product.models import Product
 
 
@@ -13,13 +13,15 @@ class DashboardStatsService:
     def get_stats():
         today = timezone.now().date()
 
-        profit_expression = ExpressionWrapper((F("sell_price") - F("product__arrival_price")) * F("quantity"),
-                                              output_field=DecimalField(max_digits=14, decimal_places=2))
+        profit_expr = ExpressionWrapper(
+            (F("price") - F("product__arrival_price")) * F("quantity"),
+            output_field=DecimalField(max_digits=14, decimal_places=2)
+        )
 
         stats = Order.objects.aggregate(
-            today_income=Coalesce(Sum(profit_expression, filter=Q(order__created_at__date=today)),
-                                  Decimal("0.00")),
-            total_income=Coalesce(Sum(profit_expression), Decimal("0.00")),
+            today_income=OrderItem.objects.filter(order__created_at__date=today).aggregate(
+                income=Coalesce(Sum(profit_expr), Decimal("0.00")))["income"],
+            total_income=OrderItem.objects.aggregate(income=Coalesce(Sum(profit_expr), Decimal("0.00")))["income"],
             total_sales=Coalesce(Count("id"), 0),
             total_discount=Coalesce(Sum("discount"), Decimal("0.00")))
         total_products = Product.objects.count()
