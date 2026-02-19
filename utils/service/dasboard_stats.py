d5e6045c929f707774590447b51bec8f9fe71895
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-19 16:55:31 +0500

    done

--- utils/service/dasboard_stats.py
+++ utils/service/dasboard_stats.py
@@ -1,4 +1,4 @@
-from django.db.models import Sum, Count, Q
+from django.db.models import Sum, F, ExpressionWrapper, DecimalField, Q, Count
 from django.db.models.functions import Coalesce
 from django.utils import timezone
 from decimal import Decimal
@@ -13,12 +13,15 @@ class DashboardStatsService:
     def get_stats():
         today = timezone.now().date()
 
+        profit_expression = ExpressionWrapper((F("price") - F("product__arrival_price")) * F("quantity"),
+                                              output_field=DecimalField(max_digits=14, decimal_places=2))
+
         stats = Order.objects.aggregate(
-            today_income=Coalesce(Sum("total_price", filter=Q(created_at__date=today)), Decimal("0.00")),
-            total_income=Coalesce(Sum("total_price"), Decimal("0.00")),
+            today_income=Coalesce(Sum(profit_expression, filter=Q(order__created_at__date=today)),
+                                  Decimal("0.00")),
+            total_income=Coalesce(Sum(profit_expression), Decimal("0.00")),
             total_sales=Coalesce(Count("id"), 0),
-            total_discount=Coalesce(Sum("discount"), Decimal("0.00")),
-        )
+            total_discount=Coalesce(Sum("discount"), Decimal("0.00")))
         total_products = Product.objects.count()
 
         return {
