9a9e3afe32ad7d37460a813efd3c82f8ee84094d
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-24 15:34:56 +0500

    done

--- acceptance/serializers.py
+++ acceptance/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Acceptance, AcceptanceHistory, CurrencyRate
+from .models import Acceptance, AcceptanceHistory
 
 
 class AcceptanceSerializer(serializers.ModelSerializer):
