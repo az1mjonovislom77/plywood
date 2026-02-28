0d10c5e4fee34841a1c071aeeb85295125e782bd
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-28 12:29:39 +0500

    done

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from product.views import ProductViewSet, QualityViewSet, show_ip
+from product.views import ProductViewSet, QualityViewSet
 
 router = DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
