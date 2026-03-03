62e4f62be2d370d415ecf1126488150546b7ac65
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-03-03 12:14:08 +0500

    done

--- utils/urls.py
+++ utils/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from utils.views import CurrencyViewSet, LowStockNotificationView, DashboardStatsView
+from utils.views import CurrencyViewSet, LowStockNotificationView, DashboardStatsView, DashboardRangeStatsAPIView
 
 router = DefaultRouter()
 router.register('currency', CurrencyViewSet, basename='currency')
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path("notifications/low-stock/", LowStockNotificationView.as_view(), name="stock-notification"),
     path("dashboard/stats/", DashboardStatsView.as_view()),
+    path("range/stats/", DashboardRangeStatsAPIView.as_view()),
 
 ]
