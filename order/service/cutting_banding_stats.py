2118feeab7806d4c459b97bc99afa6e536941f5d
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-03-04 11:35:55 +0500

    done

--- order/service/cutting_banding_stats.py
+++ order/service/cutting_banding_stats.py
@@ -1,8 +1,8 @@
-from django.db.models import Sum, F, DecimalField, ExpressionWrapper, Q, Value
+from django.db.models import Sum, F, DecimalField, ExpressionWrapper, Q
 from django.db.models.functions import Coalesce
 from django.utils import timezone
 from decimal import Decimal
-from order.models import Order
+from order.models import Order, Banding, Cutting
 
 
 class DashboardStatsService:
@@ -12,23 +12,44 @@ class DashboardStatsService:
         today = timezone.now().date()
 
         cutting_expr = ExpressionWrapper(
-            F("cutting__price") * F("cutting__count"),
+            F("price") * F("count"),
             output_field=DecimalField(max_digits=14, decimal_places=2)
         )
 
         banding_expr = ExpressionWrapper(
-            Coalesce(F("banding__length"), Value(0)) *
-            Coalesce(F("banding__thickness__price"), Value(0)),
+            F("length") * F("thickness__price"),
             output_field=DecimalField(max_digits=14, decimal_places=2)
         )
 
-        stats = Order.objects.aggregate(
+        cutting_stats = Cutting.objects.aggregate(
             total_cutting_income=Coalesce(Sum(cutting_expr), Decimal("0.00")),
-            today_cutting_income=Coalesce(Sum(cutting_expr, filter=Q(created_at__date=today)), Decimal("0.00")),
-            total_banding_income=Coalesce(Sum(banding_expr), Decimal("0.00")),
-            today_banding_income=Coalesce(Sum(banding_expr, filter=Q(created_at__date=today)), Decimal("0.00")), )
+            today_cutting_income=Coalesce(
+                Sum(cutting_expr, filter=Q(created_at__date=today)),
+                Decimal("0.00")
+            ),
+        )
 
-        stats["total_income"] = (stats["total_cutting_income"] + stats["total_banding_income"])
-        stats["today_income"] = (stats["today_cutting_income"] + stats["today_banding_income"])
+        banding_stats = Banding.objects.aggregate(
+            total_banding_income=Coalesce(Sum(banding_expr), Decimal("0.00")),
+            today_banding_income=Coalesce(
+                Sum(banding_expr, filter=Q(created_at__date=today)),
+                Decimal("0.00")
+            ),
+        )
+
+        stats = {
+            **cutting_stats,
+            **banding_stats
+        }
+
+        stats["total_income"] = (
+            stats["total_cutting_income"] +
+            stats["total_banding_income"]
+        )
+
+        stats["today_income"] = (
+            stats["today_cutting_income"] +
+            stats["today_banding_income"]
+        )
 
         return stats
