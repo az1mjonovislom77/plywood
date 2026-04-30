98a94ca31bba7a506b655ccf6e8f68672d11cb21
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-04-30 09:32:20 +0500

    done

--- utils/api/urls.py
+++ utils/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from utils.api.views.dashboard import DashboardStatsView, ComprehensiveDashboardStatsAPIView
+from utils.api.views.dashboard import ComprehensiveDashboardStatsAPIView
 from utils.api.views.expenses import ExpenseViewSet, ExpenseHistoryViewSet, CashFlowReportExcelViewSet
 from utils.api.views.utils import CurrencyViewSet, LowStockNotificationView
 
@@ -12,7 +12,6 @@ router.register("history-expenses", ExpenseHistoryViewSet, basename='expenses-hi
 urlpatterns = [
     path('', include(router.urls)),
     path("notifications/low-stock/", LowStockNotificationView.as_view(), name="stock-notification"),
-    path("dashboard/stats/", DashboardStatsView.as_view()),
     path("all-stats/", ComprehensiveDashboardStatsAPIView.as_view()),
     path("export/", CashFlowReportExcelViewSet.as_view({"get": "list"}), name="cashflow-report")
 ]
