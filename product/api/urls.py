a2537b87109d0354c76ea9c73b6bb0f6b85def20
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-04-30 09:48:35 +0500

    done

--- product/api/urls.py
+++ product/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from product.api.views.product_views import ProductViewSet, MaterialReportExcelViewSet
+from product.api.views.product_views import ProductViewSet, MaterialReportExcelViewSet, MaterialReportJsonViewSet
 from product.api.views.quality_views import QualityViewSet
 
 router = DefaultRouter()
@@ -9,6 +9,7 @@ router.register('quality', QualityViewSet, basename='quality')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path("export/", MaterialReportExcelViewSet.as_view({"get": "list"}), name="material-report")
+    path("export/", MaterialReportExcelViewSet.as_view({"get": "list"}), name="material-report"),
+    path("product-report/", MaterialReportJsonViewSet.as_view({"get": "list"})),
 
 ]
--- utils/api/urls.py
+++ utils/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from utils.api.views.dashboard import ComprehensiveDashboardStatsAPIView
-from utils.api.views.expenses import ExpenseViewSet, ExpenseHistoryViewSet, CashFlowReportExcelViewSet
+from utils.api.views.expenses import ExpenseViewSet, ExpenseHistoryViewSet, CashFlowReportExcelViewSet, \
+    FinanceReportJsonViewSet
 from utils.api.views.utils import CurrencyViewSet, LowStockNotificationView
 
 router = DefaultRouter()
@@ -13,5 +14,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path("notifications/low-stock/", LowStockNotificationView.as_view(), name="stock-notification"),
     path("all-stats/", ComprehensiveDashboardStatsAPIView.as_view()),
-    path("export/", CashFlowReportExcelViewSet.as_view({"get": "list"}), name="cashflow-report")
+    path("export/", CashFlowReportExcelViewSet.as_view({"get": "list"}), name="cashflow-report"),
+    path("finance-report/", FinanceReportJsonViewSet.as_view({"get": "list"})),
 ]
