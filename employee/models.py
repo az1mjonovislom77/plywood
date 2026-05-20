f852c1f9b86df93a284a18e605c861d5ccbca1a0
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-05-20 14:14:49 +0500

    done

--- employee/models.py
+++ employee/models.py
@@ -5,7 +5,6 @@ class Employee(models.Model):
     full_name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=20)
     salary = models.DecimalField(max_digits=20, decimal_places=2)
-    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
 
     class Meta:
         ordering = ['full_name']
