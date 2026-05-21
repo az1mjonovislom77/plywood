4afdaa0db4c82d1bcc384196ebe7c6b1985f1777
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-05-21 08:14:52 +0500

    done

--- employee/api/urls.py
+++ employee/api/urls.py
@@ -8,9 +8,9 @@ router = DefaultRouter()
 router.register('', EmployeeViewSet, basename='employee')
 
 urlpatterns = [
-    path('', include(router.urls)),
     path("pay/", PaySalaryAPIView.as_view(), name="pay-salary"),
     path("<int:employee_id>/history/", EmployeeSalaryHistoryAPIView.as_view(), name="salary-history"),
     path("<int:employee_id>/monthly/", EmployeeSalaryMonthlyReportAPIView.as_view(), name="salary-monthly"),
     path("totals/", AllEmployeesTotalSalaryAPIView.as_view(), name="all-employees-total"),
+    path('', include(router.urls)),
 ]
