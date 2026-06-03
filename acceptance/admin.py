7e0898cc0ce98bfc170fbe060d250fc51e0c5c13
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-06-03 10:10:39 +0500

    done

--- acceptance/admin.py
+++ acceptance/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 
 class AcceptanceAdmin(admin.ModelAdmin):
-    list_display = ("id", "supplier", "product")
+    list_display = ("id", "supplier", "product", "count")
     list_select_related = ("supplier", "product")
 
 
