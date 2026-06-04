bcbed693e8ec96b75dac8a1bf3962bd37a4ad293
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-06-04 16:15:41 +0500

    done

--- product/api/views/profit_views.py
+++ product/api/views/profit_views.py
@@ -11,6 +11,7 @@ from product.services.export_json import MaterialReportJsonService
 from acceptance.models import CurrencyRate
 from order.models import OrderItem
 from category.models import Category
+from product.services.total_profit import AllProfitService
 from utils.models import Services, ServicesName
 
 
@@ -222,7 +223,7 @@ class KromkaProfitView(APIView):
             product_profit_dollar += profit_dollar
             product_profit_som += profit_som
 
-        from services.total_profit import AllProfitService
+
         all_profit = AllProfitService.calculate(
             date_from=date_from,
             date_to=date_to,
