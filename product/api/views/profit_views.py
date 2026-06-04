1439baa9ebca24fd6a91f496d46a9f11b98a35a2
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-06-04 17:39:53 +0500

    done

--- product/api/views/profit_views.py
+++ product/api/views/profit_views.py
@@ -4,7 +4,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.utils import timezone
-
 from category.models import Category
 from product.services.ancillary_profit import AncillaryProfitService
 from product.services.material_profit import KROMKA_CATEGORY_NAME, MaterialProfitService
@@ -86,11 +85,6 @@ class KromkaProfitView(APIView):
         product_profit_som, product_profit_dollar, products_count = (
             MaterialProfitService.calc_kromka_product_profit(context)
         )
-        banding_som, banding_dollar = AncillaryProfitService.calc_banding_profit(
-            context["start_dt"],
-            context["end_dt"],
-            context["rate_value"],
-        )
         all_profit = AllProfitService.calculate(
             date_from=date_from,
             date_to=date_to,
@@ -106,9 +100,5 @@ class KromkaProfitView(APIView):
             "kromka_product_profit_som": float(product_profit_som),
             "kromka_product_profit_dollar": float(product_profit_dollar),
             "kromka_products_count": products_count,
-            "kromka_xizmat_profit_som": float(banding_som),
-            "kromka_xizmat_profit_dollar": float(banding_dollar),
-            "kromka_total_profit_som": float(product_profit_som + banding_som),
-            "kromka_total_profit_dollar": float(product_profit_dollar + banding_dollar),
             **all_profit,
         })
--- product/tests.py
+++ product/tests.py
@@ -1,14 +1,7 @@
 from decimal import Decimal
 from django.test import TestCase
-from django.utils import timezone
-
-from acceptance.models import CurrencyRate
-from category.models import Category
-from order.models import Banding, Order, OrderItem
 from product.api.serializers import ProductSerializer
 from product.models import Product
-from product.services.ancillary_profit import AncillaryProfitService
-from product.services.material_profit import MaterialProfitService
 
 
 class ProductSerializerTest(TestCase):
@@ -18,62 +11,3 @@ class ProductSerializerTest(TestCase):
         data = ProductSerializer(product).data
 
         self.assertEqual(data["count"], "25")
-
-
-class BandingProfitTest(TestCase):
-    def setUp(self):
-        today = timezone.localdate()
-        CurrencyRate.objects.create(date=today, rate=Decimal("12500.00"))
-        self.kromka = Category.objects.create(name="KROMKA")
-        self.product = Product.objects.create(
-            name="Kromka roll",
-            category=self.kromka,
-            sale_price=Decimal("100000.00"),
-            count=Decimal("10.000"),
-        )
-        self.order = Order.objects.create(
-            order_status=Order.OrderStatus.ACCEPT,
-            payment_method=Order.PaymentMethod.NASIYA,
-            covered_amount=Decimal("0"),
-            accepted_at=timezone.now(),
-        )
-        self.banding = Banding.objects.create(
-            thickness=Decimal("100000.00"),
-            length=Decimal("5.00"),
-            payment_method=Banding.PaymentMethod.NASIYA,
-            covered_amount=Decimal("0"),
-        )
-        OrderItem.objects.create(
-            order=self.order,
-            product=self.product,
-            quantity=Decimal("1"),
-            price=Decimal("0"),
-            banding=self.banding,
-        )
-
-    def test_banding_profit_counts_service_amount_not_payment(self):
-        today = timezone.localdate()
-        context = MaterialProfitService.build_profit_context(
-            str(today), str(today)
-        )
-        banding_som, _ = AncillaryProfitService.calc_banding_profit(
-            context["start_dt"],
-            context["end_dt"],
-            context["rate_value"],
-        )
-        expected = Decimal("500000.00")
-        self.assertEqual(banding_som, expected)
-
-    def test_kromka_total_includes_product_and_banding(self):
-        today = timezone.localdate()
-        context = MaterialProfitService.build_profit_context(
-            str(today), str(today)
-        )
-        product_som, _, _ = MaterialProfitService.calc_kromka_product_profit(context)
-        banding_som, _ = AncillaryProfitService.calc_banding_profit(
-            context["start_dt"],
-            context["end_dt"],
-            context["rate_value"],
-        )
-        self.assertEqual(banding_som, Decimal("500000.00"))
-        self.assertGreaterEqual(product_som + banding_som, Decimal("500000.00"))
